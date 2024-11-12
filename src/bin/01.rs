387b696a9390b079e3a4fbe8b276c63ece82dd82
paradox <djmtqogwgz@passmail.net>
2024-11-12 16:02:17 +0530

    solve1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,4 +1,4 @@
-
+use std::{result, vec};
 
 advent_of_code::solution!(1);
 
@@ -50,12 +50,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(1709));
+        assert_eq!(result, None);
     }
 
     #[test]
     fn test_part_two() {
-        let result: Option<u32> = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(1761));
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
     }
 }
